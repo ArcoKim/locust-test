a46a6377d5fd27a1dfaa7382fc79d0e5896838fa
ArcoKim <arcomade12@gmail.com>
2024-12-19 23:58:08 +0900

    few error fix & add "bot-attack" user-agent

--- locustfile.py
+++ locustfile.py
@@ -2,6 +2,7 @@ from locust import FastHttpUser, task
 from faker import Faker
 import pandas as pd
 import random
+import threading
 
 fake = Faker()
 
@@ -9,32 +10,54 @@ emp = pd.read_csv('data/employees.csv')
 first_names = emp["first_name"].tolist()
 last_names = emp["last_name"].tolist()
 
+write_idx = 500000
+read_idx = 0
+index_lock = threading.Lock()
+
 class TestUser(FastHttpUser):
-    write_num = 500000
-    read_num = 0
-
-    @task
+    @task(2)
     def token(self):
-        self.client.post("/v1/token?id=world&uuid=skills", json={"length": 256})
+        self.client.post("/v1/token?id=world&uuid=skills", json={"length": 512})
 
-    @task
-    def employee(self):
+    @task(2)
+    def write_employee(self):
+        global write_idx
+        with index_lock:
+            write_now = write_idx
+            write_idx += 1
+
         birth_date = fake.date_of_birth()
         first_name = fake.first_name()
         last_name = fake.last_name()
 
         self.client.post("/v1/employee?id=world&uuid=skills", json={
-            "emp_no": self.write_num,
+            "emp_no": write_now,
             "birth_date": birth_date.isoformat(),
             "first_name": first_name,
             "last_name": last_name,
-            "gender": random.choice('M', 'F'),
-            "hire_date": fake.date_between_dates(date_start=birth_date)
+            "gender": random.choice(['M', 'F']),
+            "hire_date": fake.date_between_dates(date_start=birth_date).isoformat()
         })
 
         first_names.append(first_name)
         last_names.append(last_name)
-        self.write_num += 1
 
-        self.client.get(f"/v1/employee?first_name={first_names[self.read_num]}&last_name={last_names[self.read_num]}&id=world&uuid=skills")
-        self.read_num += 1
+    @task(2)
+    def read_employee(self):
+        global read_idx
+        with index_lock:
+            read_now = read_idx
+            read_idx += 1
+
+        self.client.get(f"/v1/employee?first_name={first_names[read_now]}&last_name={last_names[read_now]}&id=world&uuid=skills")
+
+    @task(1)
+    def bad_token(self):
+        self.client.post("/v1/employee?id=world&uuid=skills", json={
+            "emp_no": 1,
+            "birth_date": "2024-12-19",
+            "first_name": "attack",
+            "last_name": "bot",
+            "gender": 'M',
+            "hire_date": "2024-12-19"
+        }, headers={"User-Agent": "bot-attack"})
